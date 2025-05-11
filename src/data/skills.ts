7baa4b0958674472a5e8f3d5502a0a6a94ba51cb
MohamedIsa <131896632+MohamedIsa@users.noreply.github.com>
2025-05-11 22:46:44 +0300

    Update skills.ts

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -4,6 +4,23 @@ export interface Skill {
 }
 
 export const devSkills: Skill[] = [
+  
+  {name: "Dart",icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg",},
+  { name: "Flutter", icon: "https://skillicons.dev/icons?i=flutter" },
+  { name: "JavaScript", icon: "https://skillicons.dev/icons?i=javascript" },
+  { name: "React", icon: "https://skillicons.dev/icons?i=react" },
+  {
+    name: "Next.js",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+  },
+  { name: "Tailwind CSS", icon: "https://skillicons.dev/icons?i=tailwind" },
+  {name: "Ionic",icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ionic/ionic-original.svg",},
+  { name: "Angular", icon: "https://skillicons.dev/icons?i=angular" },
+  { name: "TypeScript", icon: "https://skillicons.dev/icons?i=typescript" },
+  {
+    name: "MySQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+  },
   {
     name: "Java",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
@@ -12,41 +29,12 @@ export const devSkills: Skill[] = [
     name: "PHP",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
   },
-  { name: "JavaScript", icon: "https://skillicons.dev/icons?i=javascript" },
-  {
-    name: "Dart",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg",
-  },
-  { name: "TypeScript", icon: "https://skillicons.dev/icons?i=typescript" },
-  {
-    name: "MySQL",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
-  },
-  {
-    name: "Node.js",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-  },
-  { name: "Firebase", icon: "https://skillicons.dev/icons?i=firebase" },
-  { name: "Flutter", icon: "https://skillicons.dev/icons?i=flutter" },
   {
     name: "Linux",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
   },
-  {
-    name: "Ionic",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ionic/ionic-original.svg",
-  },
-  { name: "Angular", icon: "https://skillicons.dev/icons?i=angular" },
-  { name: "Tailwind CSS", icon: "https://skillicons.dev/icons?i=tailwind" },
-  {
-    name: "Next.js",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
-  },
-
-  { name: "React", icon: "https://skillicons.dev/icons?i=react" },
-  { name: "Vite", icon: "https://skillicons.dev/icons?i=vite" },
-  {
+  { name: "Firebase", icon: "https://skillicons.dev/icons?i=firebase" },
+{
     name: "Google Sites",
     icon: "https://fonts.gstatic.com/s/i/productlogos/sites_2020q4/v3/web-48dp/logo_sites_2020q4_color_2x_web_48dp.png",
-  },
-];
+  },];
