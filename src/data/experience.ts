a7d0cdef92701bb281b87e7b0cbf075d613d9ea7
MohamedIsa <131896632+MohamedIsa@users.noreply.github.com>
2025-05-11 22:46:24 +0300

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -23,16 +23,16 @@ export const experienceSection: ExperienceSection = {
       year: "July-2024 - Sept-2024",
       description:
         "Developing a cross-platform and full-stack website and mobile app using Ionic-Angular and Laravel for the backend.",
-      skills: ["Ionic", "Angular", "Laravel", "TypeScript", "API Integration"]
+      skills: ["Ionic", "Angular", "Laravel", "MySQL", "Firebase Storage"]
     },
     {
       id:2,
       title:"Web Developer Intern",
       company: "Ministry of Justice, Islamic Affairs and Waqf",
-      year: "March-2024 - OnGoing",
+      year: "March-2025 - May-2025",
       description:
         "Google Sites was used to turn a book into a website, creating and organizing pages for reading and ease of navigation while guaranteeing device responsiveness and accessibility.",
-      skills: ["Google Sites", "HTML/CSS", "UI/UX", "Content Management"]
+      skills: ["Google Sites"]
     },
   ],
 };
